8507414f8f0c9be062490c628be20efa5669d269
HAN2496 <137390470+HAN2496@users.noreply.github.com>
2024-06-04 16:55:58 +0900

    1

---
+++ MLP7/control4.py
@@ -0,0 +1,194 @@
+import os
+import pandas as pd
+import numpy as np
+from subject import Subject
+from gp import GP
+from matplotlib import pyplot as plt
+from datasets import Datasets
+from scipy.optimize import minimize
+import imageio
+from matplotlib import animation
+# import matplotlib
+# matplotlib.use('Agg')
+
+class Control:
+    def __init__(self):
+        self.subject = Subject(6, cut=True)
+        self.gp = GP(self.subject)
+        self.original_datas = self.subject.datas
+        self.dt = self.original_datas['header'][1] - self.original_datas['header'][0]
+        self.total_data_num = len(self.original_datas['header'])
+        print("Total data num: ", self.total_data_num)
+        self.original_datas.appends({'start_indices': self.subject.start_indices})
+        self.corrected_datas = Datasets()
+
+        self.Kp = 0
+        self.Ki = 0
+        self.Kd = 0
+        self.learning_rate = 0.001
+        self.num_iterations = 201
+
+    def control(self):
+        one_more = True
+        idx = 0
+        interval_predict = 10
+        filenames = []
+        i_error = 0
+        d_error = 0
+        while idx < self.total_data_num - interval_predict:
+            if self.original_datas['section'][idx] == 0:
+                self.corrected_datas.appends(self.original_datas.indexs(idx))
+                idx += 1
+            else:
+                if one_more:
+                    self.corrected_datas.appends(self.original_datas.indexs(idx))
+                    idx += 1
+                    one_more = False
+                    self.gp._init(self.original_datas['header'][:idx], self.original_datas['heelstrike'][:idx],
+                                  self.original_datas['heelstrike_x'][:idx], self.original_datas['heelstrike_y'][:idx])
+                    continue
+                print(f"Now idx: {idx} (total: {self.total_data_num})")
+
+                x_scale, y_scale = self.minimize_scale(idx)
+                print(f"x scale: {x_scale} / y scale: {y_scale}")
+
+        
+                X_pred_gp, y_pred_gp = self.gp.scale(self.corrected_datas['heelstrike'][-1], x_scale, y_scale,
+                                                     self.corrected_datas['header'][-1], end=False)
+                tmp_x, tmp_y = X_pred_gp, y_pred_gp
+
+                torque_subject = self.original_datas['torque'][idx:idx+interval_predict]
+                a_t_prev = self.corrected_datas['hip_sagittal_acc'][-1]
+                v_t = self.corrected_datas['hip_sagittal_speed'][-1]
+                x_t = self.corrected_datas['hip_sagittal'][-1]
+
+                for i in range(interval_predict):
+                    error =  y_pred_gp[i+1] - self.corrected_datas['hip_sagittal'][-1]
+                    d_error = error / self.dt
+                    i_error += error * self.dt
+                    total_error =  self.Kp * error + self.Kd * d_error + self.Ki * i_error
+                    torque_input = total_error + torque_subject[i] * 0.5
+
+                    # print(f"error: {error}, subject torque: {torque_subject[i]}, torque_input: {torque_input}" )
+
+                    a_t = self.subject.move(torque_input=torque_input)[0]
+                    a_t1 = a_t
+                    v_t1 = v_t + a_t * self.dt
+                    x_t1 = x_t + v_t1 * self.dt + 0.5 * a_t * self.dt ** 2
+
+                    # a_t1 = (a_t + a_t_prev) / 2
+                    # v_t1 = v_t + a_t1 * self.dt
+                    # x_t1 = x_t + v_t * self.dt + 0.5 * a_t_prev * self.dt ** 2
+
+
+                    plt.figure(figsize=(15, 10))
+                    plt.ylim([-30, 30])
+                    plt.plot(self.original_datas['header'], self.original_datas['hip_sagittal'],  color='green', label='original subject')
+                    plt.plot(self.corrected_datas['header'], self.corrected_datas['hip_sagittal'], marker='.', linestyle='', color='k', label='corrected subject')
+                    X_pred_gp, y_pred_gp = self.gp.scale(self.corrected_datas['heelstrike'][-1], x_scale, y_scale, self.corrected_datas['header'][-1])
+                    plt.plot(X_pred_gp, y_pred_gp, color='blue', label='gp before')
+                    plt.text(self.corrected_datas['header'][-1]+0.5, self.corrected_datas['hip_sagittal'][-1], error)
+                    plt.text(self.corrected_datas['header'][-1]+0.5, self.corrected_datas['hip_sagittal'][-1] - 1, total_error)
+                    plt.text(self.corrected_datas['header'][-1]+0.5, self.corrected_datas['hip_sagittal'][-1] - 2, torque_subject[i])
+                    half_len = int(len(tmp_y))
+                    if i <= 1:
+                        plt.plot(tmp_x[:half_len], tmp_y[:half_len], color='red', label='gp after')
+                    else:
+                        plt.plot(tmp_x[:half_len], tmp_y[:half_len], color='red', marker='.', linestyle='', label='gp after')
+                    plt.legend()
+                    filename = f"tmp/{idx}.png"
+                    filenames.append(filename)
+                    plt.savefig(filename)
+                    plt.close()
+
+                    data_point = {
+                        'section': self.original_datas['section'][idx],
+                        'header': self.original_datas['header'][idx],
+                        'hip_sagittal': x_t1,
+                        'hip_sagittal_speed': v_t1,
+                        'hip_sagittal_acc': a_t1,
+                        'heelstrike': self.original_datas['heelstrike'][idx],
+                        'heelstrike_x': self.original_datas['heelstrike_x'][idx],
+                        'heelstrike_y': self.original_datas['heelstrike_y'][idx],
+                        'torque': torque_input
+                    }
+                    self.corrected_datas.appends(data_point)
+                    x_t = x_t1
+                    v_t = v_t1
+                    a_t_prev = a_t
+
+                    idx += 1
+
+        frames = []
+        print('now gif will save ...')
+        existing_gif_count = sum(1 for file in os.listdir("image/") if file.startswith("output") and file.endswith(".gif"))
+        next_gif_number = existing_gif_count + 1
+        exportname = f"image/output{next_gif_number}_Kp{self.Kp}_Ki{self.Ki}_Kd_{self.Kd}.gif"
+        duration_rate = 1
+        for filename in filenames:
+            if filename.endswith(".png"):
+                frames.append(imageio.imread(filename))
+        imageio.mimsave(exportname, frames, format='GIF', duration=duration_rate)
+        print(exportname, filenames[-1])
+        # plt.savefig(exportname, filenames[-1])
+
+        # for filename in set(filenames):
+        #     os.remove(filename)
+        print('gif saved.')
+
+    def minimize_scale(self, idx):
+        def objective(params, X, y_actual, heelstrike, interval=0):
+            tmp = 0
+            scale_x, scale_y = params
+            #print(f"x scale: {scale_x} / y scale: {scale_y}")
+            X_pred, y_pred = self.gp.scale(heelstrike, scale_x, scale_y, X[-1])
+            error = 0
+            for x, y in zip(X_pred, y_pred):
+                if x < X[0]:
+                    continue
+                distances = np.abs(x - X)
+                closest_idxs = np.argsort(distances)[0]
+                error += np.mean(np.square(y - self.corrected_datas['hip_sagittal'][closest_idxs]))
+            if tmp % 20 == 0:
+                # plt.plot(X_pred, y_pred, color='red')
+                # plt.plot(self.corrected_datas['header'], y_actual, color='k')
+                # plt.plot(self.original_datas['header'], self.original_datas['hip_sagittal'], color='b', linestyle='--')
+                # plt.pause(0.001)
+                # plt.cla()
+                pass
+            tmp+=1
+            return error
+        initial_params = [1.0, 1.0]
+
+        X = self.corrected_datas['header']
+        y = self.corrected_datas['hip_sagittal']
+        #bounds = [(0.1, 10.0), (0.5, 1.5)]
+        result = minimize(objective, initial_params, args=(X, y, self.corrected_datas['heelstrike'][-1]), method='L-BFGS-B')#, bounds=bounds)
+        if result.success:
+            optimized_scale_x, optimized_scale_y = result.x
+            #print("Optimization successful:", result.message)
+            # print("Optimized scales:", optimized_scale_x, optimized_scale_y)
+        else:
+            print("Optimization failed:", result.message)
+            optimized_scale_x, optimized_scale_y = result.x
+            #raise ValueError("Optimization failed:", result.message)
+
+        return result.x
+
+    def plot(self, show=True):
+        x = self.original_datas['header']
+        plt.plot(x, self.original_datas['hip_sagittal'], label='original trajectory')
+        plt.plot(x, self.gp.y_pred_original, label='gp original')
+        for idx in self.original_datas['start_indices']:
+            plt.axvline(self.original_datas['header'][idx], color='black', linestyle='--')
+
+        if show:
+            plt.legend()
+            plt.show()
+
+control = Control()
+control.control()
+#control.plot()
+#for idx in range(control.total_data_num - 1):
+#for idx in range(150):
+#    control.control(idx)
---
+++ sucess/MLP6/gp.py
@@ -0,0 +1,122 @@
+import numpy as np
+from joblib import load
+from subject import Subject
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+
+class GP:
+    def __init__(self, subject, path='gaussian_process_regressor.joblib'):
+        self.subject = subject
+        self.data = subject.datas
+        self.X_time = self.data['header']
+        self.data_num = len(self.data)
+        self.path = path
+        self.model = load(self.path)
+        self.predict_by_heelstrike()
+        self.X_original = self.X.copy()
+        self.X_scalar_original = self.X_scalar.copy()
+        self.X_time_original = self.X_time.copy()
+        self.y_pred_original = self.y_pred.copy()
+        self.sigma_original = self.sigma.copy()
+
+    def predict(self, X, save_data=True): #polar coordinate으로 predict 뽑는게 디폴트
+        X = np.array(X)
+        y_pred, sigma = self.model.predict(X, return_std=True)
+        if save_data:
+            self.X = X
+            self.y_pred = y_pred
+            self.sigma = sigma
+        return y_pred, sigma
+
+    def predict_by_heelstrike(self, start=0, end=100, save_data=True): #scalar 값으로 predict 계산시 사용
+        HeelStrike = self.data['heelstrike']
+        self.X_scalar = HeelStrike
+        heel_strike_x = self.data['heelstrike_x']
+        heel_strike_y = self.data['heelstrike_y']
+        X = np.column_stack((heel_strike_x, heel_strike_y))
+        return self.predict(X, save_data=save_data)
+
+    def find(self, pos):
+        return self.y_pred[pos]
+
+    def find_pred_value_by_heelstrike(self, x_scalar, section): # (HeelStrike) Scalar 값으로 gp 모델의 predict 값 반환
+        start_idx, end_idx = self.subject.heel_strike_indices[section:section+2]
+        if x_scalar in self.X_scalar[start_idx:end_idx]:
+            return self.y_pred[np.where(x_scalar == self.X_scalar[start_idx:end_idx])]
+        else:
+            closest_indices = np.argsort(np.abs(self.X_scalar - x_scalar))[:2]
+            closest_x_values = self.X_scalar[closest_indices]
+            closest_y_values = self.y_pred[closest_indices]
+            x0, x1 = closest_x_values
+            y0, y1 = closest_y_values
+            interpolated_value = y0 + (y1 - y0) * (x_scalar - x0) / (x1 - x0)
+            return interpolated_value
+
+
+    def translation(self, delx=0, dely=0, save=True):
+        if save:
+            self.X_scalar += delx
+            self.y_pred -= dely
+        else:
+            return self.X_scalar + delx, self.y_pred - dely
+
+    def update(self, datas):
+        heel_strike_x = datas['heelstrike_x']
+        heel_strike_y = datas['heelstrike_y']
+        X = np.column_stack((heel_strike_x, heel_strike_y))
+        self.X = X
+        self.y_pred = self.predict(X)
+
+    def scale(self, scale_x=1.0, scale_y=1.0, x_pos=0, y_pos=0, save_data=False, idx=-1):
+        
+        if x_pos != 0:
+            X_scalar_gp = self.X_scalar - x_pos
+            X_scalar_gp *= scale_x
+            X_scalar_gp += x_pos
+        else:
+            X_scalar_gp = self.X_scalar * scale_x
+        if y_pos != 0:
+            y_pred_gp = self.y_pred - y_pos
+            y_pred_gp *= scale_y
+            y_pred_gp += y_pos
+        else:
+            y_pred_gp = self.y_pred * scale_y
+
+        # if scale_x != 1.0:
+        #     interpolator = interp1d(X_scalar_gp, y_pred_gp, kind='linear', bounds_error=False, fill_value="extrapolate")
+        #     y_pred_gp = interpolator(self.X_time_original)
+
+        # if scale_x != 1.0:
+        #     cutted_datas = []
+        #     interpolated_y = []
+        #     for section in self.data['total_sections']:
+        #         idxs = self.data.sections(section, index=True)
+        #         cutted_datas.append([X_scalar_gp[idxs], y_pred_gp[idxs], self.X_scalar_original[idxs]])
+        #     for x, y, original in cutted_datas:
+        #         interpolator = interp1d(x, y, kind='linear', bounds_error=False, fill_value="extrapolate")
+        #         y_pred = interpolator(original)
+        #         interpolated_y.extend(y_pred)
+        #     y_pred_gp = np.array(interpolated_y)
+        #     if idx != -1:
+        #         #y_diff = y_pos - y_pred_gp[idx-1]
+        #         #y_pred_gp = y_pred_gp - y_diff
+        #         pass
+
+        # plt.plot(self.X_time, self.y_pred_original, color='b')
+        # plt.plot(self.X_time, y_pred_gp, color='r')
+        # plt.show()
+
+        if save_data == False:
+            if idx != -1:
+                return X_scalar_gp[:idx], y_pred_gp[:idx]
+            else:
+                return X_scalar_gp, y_pred_gp
+        else:
+            #self.X_scalar = X_scalar_gp
+            self.y_pred = y_pred_gp
+        
+
+if __name__ == "__main__":
+    subject = Subject(6, cut=True)
+    gp = GP(subject)
+    gp.scale(scale_x=1.1)
